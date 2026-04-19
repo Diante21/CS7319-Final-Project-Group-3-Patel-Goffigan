f285cdd5c1a64c7c016082693bc1ae617e8629af
Diante <DianteGoffigan>
2026-04-19 13:01:40 -0500

    Folder name change

--- backend-pipefilter/src/main/java/com/pipefilter/backend/backend_pipefilter/filter/Filter.java
+++
@@ -1,7 +0,0 @@
-package com.pipefilter.backend.backend_pipefilter.filter;
-
-import com.pipefilter.backend.backend_pipefilter.domain.ResumePayload;
-
-public interface Filter {
-    ResumePayload process(ResumePayload payload);
-}
--- backend-pipefilter/src/main/java/com/pipefilter/backend/backend_pipefilter/repository/EvaluationResultRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pipefilter.backend.backend_pipefilter.repository;
-import com.pipefilter.backend.backend_pipefilter.domain.EvaluationResult;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EvaluationResultRepository extends JpaRepository<EvaluationResult, Long> {
-
-}
